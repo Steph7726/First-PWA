295990c2d68b4815793fb14bf7d3b13245daddd7
Steph7726 <Steph7726@users.noreply.github.com>
2025-01-25 15:33:12 +0000

    deploy: e5b04a61b30f1068647a40b8612db59e28441ed1

--- app.js
+++ app.js
@@ -1,21 +1,111 @@
+import { initializeApp } from "firebase/app";
+import {
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  getFirestore,
+  collection,
+  deleteDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3v10UFzZ_HGbIIJwItHDiCdnZPBjnbZ8",
+  authDomain: "first-pwa-fd641.firebaseapp.com",
+  projectId: "first-pwa-fd641",
+  storageBucket: "first-pwa-fd641.firebasestorage.app",
+  messagingSenderId: "1026685908541",
+  appId: "1:1026685908541:web:1f145ccf10a80e7f295c54",
+  measurementId: "G-E55YXDLFSV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
+function sanitizeInput(input) {
+  const div = document.createElement("div");
+  div.textContent = input;
+  return div.innerHTML;
+}
+
 // Add Task
-addTaskBtn.addEventListener("click", () => {
+
+addTaskBtn.addEventListener("click", async () => {
   const task = taskInput.value.trim();
   if (task) {
-    const li = document.createElement("li");
-    li.textContent = task;
-    taskList.appendChild(li);
+    const taskText = sanitizeInput(task);
+
+    // Saving the to-do list
+    await addTaskToFirestore(taskText);
+    renderTasks();
     taskInput.value = "";
   }
 });
 
+async function addTaskToFirestore(taskText) {
+  await addDoc(collection(db, "todos"), {
+    text: taskText,
+    completed: false,
+  });
+}
+
+// Retrieving to-do list
+async function renderTasks() {
+  var tasks = await getTaskFromFirestore();
+  taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    if (!task.data().completed) {
+      const taskItem = document.createElement("li");
+      taskItem.id = task.id;
+      taskItem.textContent = task.data().text;
+      taskList.appendChild(taskItem);
+    }
+  });
+}
+
+async function getTaskFromFirestore() {
+  var data = await getDocs(collection(db, "todos"));
+  let userData = [];
+  data.forEach((doc) => {
+    userData.push(doc);
+  });
+  return userData;
+}
+
 // Remove Task on Click
-taskList.addEventListener("click", (e) => {
+taskList.addEventListener("click", async (e) => {
   if (e.target.tagName === "LI") {
-    e.target.remove();
+    const taskId = e.target.id;
+    await deleteTaskFromFirestore(taskId); // delete from firestore
+    e.target.remove(); // remove from UI
   }
 });
+
+// delete task from firestore
+async function deleteTaskFromFirestore(taskId) {
+  const taskRef = doc(db, "todos", taskId);
+  await deleteDoc(taskRef);
+}
+
+const sw = new URL("service-worker.js", import.meta.url);
+if ("serviceWorker" in navigator) {
+  const s = navigator.serviceWorker;
+  s.register(sw.href, {
+    scope: "/First-PWA/",
+  })
+    .then((_) =>
+      console.log(
+        "Service Worker Registered for scope:",
+        sw.href,
+        "with",
+        import.meta.url
+      )
+    )
+    .catch((err) => console.error("Service Worker Error:", err));
+}
